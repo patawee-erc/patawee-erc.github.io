57d751c6b4924ca4a152f5cb88466f2ea7e540c4
patawee-erc <patawee.sabo@gmail.com>
2026-03-12 09:09:03 +0700

    Update api-config.js

--- New_Face_Scan/js/api-config.js
+++ New_Face_Scan/js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = '';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwVsfpj7gxrROANTYEgVuFZ5i-ZmpVz1YTUBWuHY0ijALmdwpEVtNThTCrNaBRytS_p/exec';
